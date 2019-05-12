7a63e34beb558c697f36b35d8f9487bfa5b2bfda
JayPe69 <37151973+JayPe69@users.noreply.github.com>
2019-05-12 19:30:21 +0200

    test return end session to check led stop

--- action-Devices_Management.py
+++ action-Devices_Management.py
@@ -32,7 +32,7 @@ def subscribe_intent_callback(hermes, intentMessage):
 
 def action_wrapper(hermes, intentMessage, conf):
     hermes.publish_continue_session(intentMessage.session_id, "OK",["JayPe69:Devices_Management"])
-    hermes.publish_end_session(intentMessage.session_id, "Terminé !")
+    return hermes.publish_end_session(intentMessage.session_id, "Terminé !")
 
 
 if __name__ == "__main__":
