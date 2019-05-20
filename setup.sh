62141f80c5b637aa89f22d1250f7c2afe75ff520
JayPe <jphilippe.novent@gmail.com>
2019-05-20 18:49:08 +0200

    use python 3 env for snips setup

--- setup.sh
+++ setup.sh
@@ -1,20 +1,35 @@
 #/usr/bin/env bash -e
 
-VENV=venv
-
-if [ ! -d "$VENV" ]
+# Copy config.ini.default if config.ini doesn't exist.
+if [ ! -e config.ini ]
 then
-
-    PYTHON=`which python3`
-
-    if [ ! -f $PYTHON ]
-    then
-        echo "could not find python"
-    fi
-    virtualenv -p $PYTHON $VENV
-
+    cp config.ini.default config.ini
 fi
 
-. $VENV/bin/activate
+PYTHON=`which python3`
+VENV=venv
 
-pip install -r requirements.txt
+if [ -f "$PYTHON" ]
+then
+
+    if [ ! -d $VENV ]
+    then
+        # Create a virtual environment if it doesn't exist.
+        $PYTHON -m venv $VENV
+    else
+        if [ -e $VENV/bin/python2 ]
+        then
+            # If a Python2 environment exists, delete it first
+            # before creating a new Python 3 virtual environment.
+            rm -r $VENV
+            $PYTHON -m venv $VENV
+        fi
+    fi
+
+    # Activate the virtual environment and install requirements.
+    . $VENV/bin/activate
+    pip3 install -r requirements.txt
+
+else
+    >&2 echo "Cannot find Python 3. Please install it."
+fi
